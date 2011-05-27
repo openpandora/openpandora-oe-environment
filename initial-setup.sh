7061526af24b4ee1548a5572c33735c62020ca2d
David-John Willis <John.Willis@Distant-earth.com>
2011-05-27 11:24:55 +0100

    INITIAL-SETUP: Start to move setup scripts to use OE-Core style layers.

--- initial-setup.sh
+++ initial-setup.sh
@@ -5,19 +5,28 @@ mkdir ${OE_ENV_TOP}/sources
 mkdir ${OE_ENV_TOP}/tmp
 mkdir ${OE_METADATA}
 mkdir ${OE_METADATA}/user.collection
-mkdir ${OE_METADATA}/openembedded.git
-mkdir ${OE_METADATA}/openpandora.oe.git
+mkdir ${OE_METADATA}/openembedded-core
+mkdir ${OE_METADATA}/meta-openembedded
+mkdir ${OE_METADATA}/meta-angstrom
+mkdir ${OE_METADATA}/meta-openpandora
 
 cd ${OE_ENV_TOP}/bitbake
 git clone git://git.openembedded.net/bitbake .
-git checkout --track -b 1.10 origin/1.10
-# Use 1.8.18 for now for users as 1.10 is not final yet.
-#git checkout -b 1.8.18
+git checkout --track -b 1.12 origin/1.12
 
-cd ${OE_METADATA}/openembedded.git
-git clone git://git.openpandora.org/openembedded.git .
-git checkout op.openembedded.dev
+cd ${OE_METADATA}/openembedded-core
+git clone git://git.openembedded.org/openembedded-core .
+git checkout master
 
-cd ${OE_METADATA}/openpandora.oe.git
-git clone git://git.openpandora.org/openpandora.oe.git .
+cd ${OE_METADATA}/meta-openembedded
+git clone git://git.openembedded.org/meta-openembedded .
+git checkout master
+
+cd ${OE_METADATA}/meta-angstrom
+git clone git://git.angstrom-distribution.org/meta-angstrom .
+git checkout master
+
+# Use my GIThub tree for the overlay for now.
+cd ${OE_METADATA}/meta-openpandora
+git clone git://github.com/djwillis/meta-openpandora.git .
 git checkout master
